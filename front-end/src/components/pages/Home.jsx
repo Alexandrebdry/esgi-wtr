bb01a107957094fad69466b1d0d40e642c25de03
Alexandrebdry <alexbaudry0@gmail.com>
2022-10-21 23:13:10 +0200

    feature/auth
    Get user information through jwt token
    display admin & seller btns & menus

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -12,6 +12,7 @@ export default function ({}) {
 
     useEffect(() => {
        if (user) setLoader(false) ;
+       console.log(user) ;
     },[user]) ;
 
     return (
--- front-end/src/services/colors.js
+++ front-end/src/services/colors.js
@@ -1,4 +1,5 @@
 export const color_red = '#D34848' ;
 export const color_red_hover = '#BC3939' ;
 export const color_white = '#F3F3F3' ;
+export const color_white_hover = '#E6E6E6' ;
 export const color_black = '#6C6C6C' ;
